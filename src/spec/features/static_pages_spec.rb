9b0fbad5e97e0ac07a5f0926ff329791e4d68acf
naniwanoshikiyan <seisyun4kita2@icloud.com>
2022-01-30 15:18:14 +0900

    test green

--- src/spec/features/static_pages_spec.rb
+++ src/spec/features/static_pages_spec.rb
@@ -5,7 +5,7 @@ describe "Static pages" do
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      expect(page).to have_content('Sample Bpp')
+      expect(page).to have_content('Sample App')
     end
   end
 end
