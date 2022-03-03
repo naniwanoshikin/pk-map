95f44fdf89287b96b829295c14169f9e928e93fd
naniwanoshikiyan <seisyun4kita2@icloud.com>
2022-03-03 20:59:19 +0900

    footer

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -28,7 +28,7 @@ class User < ApplicationRecord
   # :lockable, :timeoutable, :trackable and :omniauthable
 
   # 6
-  validates :name,  presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
